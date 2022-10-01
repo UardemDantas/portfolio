2a52af01924d8c7cb70ba0e26692110cec70ebc7
Uardem Dantas <uardemj12@gmail.com>
2022-10-01 03:19:41 -0300

    Changed CV Name

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,13 +3,12 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href='/assets/cv.pdf' className="btn">
+      <a href="/assets/Uardem_Curriculo.pdf" download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
         Vamos conversar !
       </a>
-      
     </div>
   );
 };
